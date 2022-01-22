135a9cf5a3c5426e2ee8230d7926ffe0b6fab964
Protomanzero0 <protomanzero01@gmail.com>
2022-01-22 01:10:58 -0700

    Finished template

---
+++ Hilo-Game-Template/hilo/game/card.py
@@ -0,0 +1,27 @@
+import random
+
+class Card:
+    """A typical playing card, numbered between 1 and 13.
+
+    The responsibility of Card is to keep track of what card is drawn. 
+
+    Attributes:
+        value (int): The value of the card drawn, between 1 and 13.
+    """
+
+
+    def __init__(self):
+        """Makes a new instance of Card.
+
+        Args:
+            self (Card): An instance of Card.
+        """
+        self.value = 0
+    
+    def draw(self):
+        """Generates a new random value for the card.
+        
+        Args:
+            self (Card): An instance of Card.
+        """
+        self.value = random.randint(1,13)
